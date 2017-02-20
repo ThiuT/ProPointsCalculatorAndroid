621c10673b0ef13be89fb2860fa1ea954097ad83
Benoît Djerigian <bdj@emakina.com>
2017-02-20 16:51:50 +0100

    Started calculator layout

---
+++ app/src/main/java/fr/tiucorps/propointscalculator/Calculator.java
@@ -0,0 +1,15 @@
+package fr.tiucorps.propointscalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+
+    // FORMULE : Lipides/4 + Glucides/9 + Protéines/11 + Fibres/30
+}
